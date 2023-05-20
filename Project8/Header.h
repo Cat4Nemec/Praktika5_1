f58d082b2a7e435f0eb4e2f764b6f56ae5c7b42e
Цайтлер Данил < @WIN-93MP03L6MQV>
2023-05-20 18:12:24 +0400

    Добавьте файлы проекта.

---
+++ Project8/Header.h
@@ -0,0 +1,10 @@
+#ifndef LOGELEM_H
+#define LOGELEM_H
+
+bool logMore(bool a, bool b);
+bool logLess(bool a, bool b);
+bool logEqul(bool a, bool b);
+bool StateConvert(bool a);
+
+#endif
+#pragma once
